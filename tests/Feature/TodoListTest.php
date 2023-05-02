775ca0b60bcf44978565cd5dbbb3159ad6c328f3
Omotolani Olurotimi <omotolaniolurotimi@gmail.com>
2023-05-02 22:53:34 +0100

    Testing

---
+++ tests/Feature/TodoListTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\TodoList;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class TodoListTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_fetch_todo_list(): void
+    {
+        // preparation /prepare
+        TodoList::create(['name' => 'my list']);
+
+        // action / perform
+        $response = $this->getJson(route('todo-list.index'));
+       
+        // assertion / predict
+        $this->assertEquals(1, count($response->json()));
+    }
+}
