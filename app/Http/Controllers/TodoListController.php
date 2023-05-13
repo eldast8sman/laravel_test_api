9d266e188db5d5ea2bb2f9be2e93a6c3d44afbbd
Omotolani Olurotimi <omotolaniolurotimi@gmail.com>
2023-05-13 17:42:42 +0100

    Continuation

--- app/Http/Controllers/TodoListController.php
+++ app/Http/Controllers/TodoListController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\TodoList;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class TodoListController extends Controller
 {
@@ -12,8 +13,24 @@ class TodoListController extends Controller
         return response($lists);
     }
 
-    public function show($id){
-        $list = TodoList::find($id);
-        return response($list);
+    public function show(TodoList $todolist){
+        return response($todolist);
+    }
+
+    public function store(Request $request){
+        $request->validate(['name' => ['required']]);
+        
+        $list = TodoList::create($request->all());
+        return response($list, 201);
+    }
+
+    public function destroy(TodoList $list){
+        $list->delete();
+        return response('', Response::HTTP_NO_CONTENT); 
+    }
+
+    public function update(Request $request, TodoList $list){
+        $list->update($request->all());
+        return $list;
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TodoListController;
 
 Route::get('todo-list', [TodoListController::class, 'index'])->name('todo-list.index');
-Route::get('todo-list/{id}', [TodoListController::class, 'show'])->name('todo-list.show');
+Route::get('todo-list/{todolist}', [TodoListController::class, 'show'])->name('todo-list.show');
+
+Route::post('todo-list', [TodoListController::class, 'store'])->name('todo-list.store');
+
+Route::delete('todo-list/{list}', [TodoListController::class, 'destroy'])->name('todo-list.destroy');
+
+Route::put('todo-list/{list}', [TodoListController::class, 'update'])->name('todo-list.update');
